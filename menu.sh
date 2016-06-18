6ee7c58ce24fa699be1e668d617f506e757ac069
alectramell <alectramell@gmail.com>
2016-06-18 00:07:00 -0700

    goldfish

---
+++ menu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+
+USERNAME=$(whoami)
+WHEREAMI=$(pwd)
+
+clear
+
+zenity --text-info --title="Goldfish v1.0" --width="500" --height="500" --html --url="https://www.google.com"
+
+clear
+
+
